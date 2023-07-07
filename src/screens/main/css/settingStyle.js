94f874cbbcf682aee1322a2b80b5d0e89e208a5f
Umerhabib-50 <umr.habib@gmail.com>
2023-07-07 18:10:13 +0500

    commit Merge branch 'rehan' of github.com:Umerhabib-50/chatApp into umer

--- src/screens/main/css/settingStyle.js
+++ src/screens/main/css/settingStyle.js
@@ -1,3 +1,38 @@
+// import {StyleSheet} from 'react-native';
+// export const SettingStyle = StyleSheet.create({
+//   tabBar: {
+//     display: 'flex',
+//     justifyContent: 'space-between',
+//     flexDirection: 'row',
+//   },
+//   roomImage: {
+//     width: 120,
+//     height: 120,
+//     borderRadius: 60,
+//     marginTop: '10%',
+//   },
+//   roomName: {
+//     color: '#ffffff',
+//     fontSize: 25,
+//     textAlign: 'center',
+//   },
+//   menuIcon: {
+//     display: 'flex',
+//     flexDirection: 'row',
+//     width: '50%',
+//     justifyContent: 'space-between',
+//     alignSelf: 'center',
+//     marginTop: '8%',
+//   },
+//   menuImage: {
+//     height: 30,
+//     width: 30,
+//   },
+//   menuText: {
+//     color: '#ffffff',
+//     marginTop: '20%',
+//   },
+// });
 import {StyleSheet} from 'react-native';
 
 export const SettingStyle = StyleSheet.create({
